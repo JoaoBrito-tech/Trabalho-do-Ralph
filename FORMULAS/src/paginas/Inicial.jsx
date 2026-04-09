c8137897ce951d23746d86d487c80a6e4d8c9e99
JoaoBrito-tech <joaopaulobritodasilva051@gmail.com>
2026-04-09 11:54:10 -0300

    Add files via upload

---
+++ FORMULAS/src/paginas/Inicial.jsx
@@ -0,0 +1,14 @@
+import estilos from './Inicial.module.css'
+import {JurosCompostos} from '../componentes/JurosCompostos'
+import {Trigonometria} from '../componentes/Trigonometria'
+import {VelocidadeMédia} from '../componentes/VelocidadeMédia'
+
+export function Inicial() {
+
+return(
+        <div className={estilos.conteiner}></div>
+
+)
+
+}
+    
