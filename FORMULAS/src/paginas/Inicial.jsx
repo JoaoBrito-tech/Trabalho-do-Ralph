1bcf464683c72969df733db60cf0e53ab0be783b
JoaoBrito-tech <joaopaulobritodasilva051@gmail.com>
2026-04-11 11:32:16 -0300

    João
    Alteração das Páginas (jsx e module.css)

--- FORMULAS/src/paginas/Inicial.jsx
+++ FORMULAS/src/paginas/Inicial.jsx
@@ -4,11 +4,30 @@ import {Trigonometria} from '../componentes/Trigonometria'
 import {VelocidadeMédia} from '../componentes/VelocidadeMédia'
 
 export function Inicial() {
+  return (
+    <div className={estilos.container}>
 
-return(
-        <div className={estilos.conteiner}></div>
+      <h1 className={estilos.titulo}>Calculadoras</h1>
 
-)
+      <div className={estilos.componentes}>
 
+        <div className={estilos.tabelas}>
+          <h2>Juros Compostos</h2>
+          <JurosCompostos />
+        </div>
+
+        <div className={estilos.tabelas}>
+          <h2>Trigonometria</h2>
+          <Trigonometria />
+        </div>
+
+        <div className={estilos.tabelas}>
+          <h2>Velocidade Média</h2>
+          <VelocidadeMédia />
+        </div>
+
+      </div>
+
+    </div>
+  )
 }
-    
